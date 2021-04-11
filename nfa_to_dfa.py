0f4284289d505ddd43ae87913e27fc1635af3c2e
b30wulffz <shlokpandey123@gmail.com>
2021-04-11 17:05:10 +0530

    Recode nfa_to_dfa algorithm

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -4,13 +4,23 @@ from collections import Counter
 from graphviz import Digraph
 import tempfile
 
-# {states: [], initial_state:"", final_states: [], alphabet: [], transition_function}
-
 def find_permutation(state_list, current_state):
     for state in state_list:
         if Counter(current_state) == Counter(state):
             return state
     return current_state
+
+def get_epsilon_closure(nfa, dfa_states, state):
+    closure_states = []
+    state_stack = [state]
+    while len(state_stack) > 0:
+        current_state = state_stack.pop(0)
+        closure_states.append(current_state)
+        alphabet = "E"
+        if nfa["transition_function"][current_state][alphabet] not in closure_states:
+            state_stack.extend(nfa["transition_function"][current_state][alphabet])
+    closure_states = tuple(set(closure_states))
+    return find_permutation(dfa_states, closure_states)
 
 def nfa_to_dfa(nfa):
     dfa = {}
@@ -19,7 +29,12 @@ def nfa_to_dfa(nfa):
     for r in range(1, len(nfa["states"])+1):
         dfa["states"].extend(itertools.combinations(nfa["states"], r))
     
-    dfa["initial_state"] = (nfa["initial_state"], )
+    # calculate epsilon closure of all states of nfa
+    epsilon_closure = {}
+    for state in nfa["states"]:
+        epsilon_closure[state] = get_epsilon_closure(nfa, dfa["states"], state)
+    dfa["initial_state"] =  epsilon_closure[nfa["initial_state"]]
+    
     dfa["final_states"] = []
     for state in dfa["states"]:
         if state != "phi":
@@ -27,38 +42,35 @@ def nfa_to_dfa(nfa):
                 if nfa_state in nfa["final_states"]:
                     dfa["final_states"].append(state)
                     break
+
     dfa["alphabets"] = ["a", "b"]
 
     dfa["transition_function"]= {}
     for state in dfa["states"]:
         dfa["transition_function"][state] = {}   
         for alphabet in dfa["alphabets"]:
-            # dfa["transition_function"][state][alphabet] = []
             if state == "phi":
                 dfa["transition_function"][state][alphabet] = state
             else:
                 transition_states = []
                 if len(state) == 1:
                     nfa_state = state[0]
-                    state_stack = [nfa_state]
-                    while len(state_stack) > 0:
-                        nfa_state = state_stack.pop(0)
-                        transition = nfa["transition_function"][nfa_state]
-                        transition_states.extend(transition[alphabet])
-                        state_stack.extend(transition["E"])
+                    next_nfa_states = nfa["transition_function"][nfa_state][alphabet]
+                    for next_nfa_state in next_nfa_states:
+                        transition_states.extend(epsilon_closure[next_nfa_state])
                 else:
                     for nfa_state in state:
                         nfa_state = tuple([nfa_state])
                         if dfa["transition_function"][nfa_state][alphabet] != "phi":
                             transition_states.extend(dfa["transition_function"][nfa_state][alphabet])
-                    transition_states = tuple(set(transition_states))
+                transition_states = tuple(set(transition_states))
 
                 if len(transition_states) == 0:
                     dfa["transition_function"][state][alphabet] = "phi"
                 else:
                     # find permutation of transition states in states
                     dfa["transition_function"][state][alphabet] = find_permutation(dfa["states"], transition_states)
-
+    
     state_stack = [dfa["initial_state"]]
     dfa["reachable_states"] = []
     while len(state_stack) > 0:
@@ -69,8 +81,6 @@ def nfa_to_dfa(nfa):
             next_state = dfa["transition_function"][current_state][alphabet]
             if next_state not in dfa["reachable_states"]:
                 state_stack.append(next_state)
-
-    # handle the case of epsilon input
 
     return dfa
 
